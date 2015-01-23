caf0a1bc60e00bacc3b09c6de5065411f6dbaa9a
Matthias Baumgartner <dev@igsor.net>
2015-01-23 11:19:44 +0100

    code review: preparations for new styles files

--- templates/header.php
+++ templates/header.php
@@ -5,6 +5,12 @@
   <link rel="stylesheet" type="text/css" href="styles/tables_old.css" />
   <link rel="stylesheet" type="text/css" href="styles/pedit_old.css" />
 
+  <!--
+  <link rel="stylesheet" type="text/css" href="styles/page.css" />
+  <link rel="stylesheet" type="text/css" href="styles/module.css" />
+  <link rel="stylesheet" type="text/css" href="styles/project.css" />
+  -->
+
   <script src="scripts/solarcalc.js"></script>
 
 </head>
