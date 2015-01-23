54650f015a9eeb47c6135552e6f278adc4e66ebf
Matthias Baumgartner <dev@igsor.net>
2015-01-23 11:17:41 +0100

    code review: file rename

--- templates/header.php
+++ templates/header.php
@@ -1,9 +1,9 @@
 <head>
   <title>Calculation Tool<?php echo $title?": $title":''; ?></title>
-  <link rel="stylesheet" type="text/css" href="styles/base.css" />
-  <link rel="stylesheet" type="text/css" href="styles/project.css" />
-  <link rel="stylesheet" type="text/css" href="styles/tables.css" />
-  <link rel="stylesheet" type="text/css" href="styles/pedit.css" />
+  <link rel="stylesheet" type="text/css" href="styles/base_old.css" />
+  <link rel="stylesheet" type="text/css" href="styles/project_old.css" />
+  <link rel="stylesheet" type="text/css" href="styles/tables_old.css" />
+  <link rel="stylesheet" type="text/css" href="styles/pedit_old.css" />
 
   <script src="scripts/solarcalc.js"></script>
 
