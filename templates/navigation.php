a9c3348702b324de5a0aa7a9d4eacede27793a51
Matthias Baumgartner <dev@igsor.net>
2015-01-24 20:52:36 +0100

    code review: keeping navi of selected page

--- templates/navigation.php
+++ templates/navigation.php
@@ -1,11 +1,20 @@
+<?php
+
+function t_menulink($trg, $name) {
+    echo "<a class='menulink " . (basename($_SERVER['SCRIPT_NAME']) == $trg ? ' selected':'') . "' href='{$trg}'>${name}</a>";
+}
+?>
+
 <div id="header">
 
   <h1 id="pagetitle">Solar Installation Calculation Tool</h1>
-  <a class="menulink" href="load.php">LOAD</a> 
-  <a class="menulink" href="panel.php">PANEL</a> 
-  <a class="menulink" href="battery.php">BATTERY</a> 
-  <a class="menulink" href="hardware.php">HARDWARE</a>
-  <a class="menulink" href="project_overview.php">PROJECT</a>
+  <?php
+    t_menulink('load.php',              'LOAD');
+    t_menulink('panel.php',             'PANEL');
+    t_menulink('battery.php',           'BATTERY');
+    t_menulink('hardware.php',          'HARDWARE');
+    t_menulink('project_overview.php',  'PROJECT');
+  ?>
   <hr class="menuline">
 
 </div>
