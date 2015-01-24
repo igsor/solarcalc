ca26eefd73f7c3ddf18452bf2cc802324ff7d036
Matthias Baumgartner <dev@igsor.net>
2015-01-24 19:17:39 +0100

    code review: acceptable color scheme?

--- styles/set_colors.sh
+++ styles/set_colors.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-sed -i -e 's/\(COL-DOM.*:\).*/\1 #0F413E;/g' *.css
-sed -i -e 's/\(COL-SUB.*:\).*/\1 #74AFAD;/g' *.css
-sed -i -e 's/\(COL-BG.*:\).*/\1 #ECECEA;/g' *.css
-sed -i -e 's/\(COL-HI.*:\).*/\1 #D9A270;/g' *.css
+sed -i -e 's/\(COL-DOM.*:\).*/\1 #1933B8;/g' *.css
+sed -i -e 's/\(COL-SUB.*:\).*/\1 #6D9EE0;/g' *.css
+#sed -i -e 's/\(COL-BG.*:\).*/\1  #E3D3CA;/g' *.css
+sed -i -e 's/\(COL-BG.*:\).*/\1  #FFFEF2;/g' *.css
+sed -i -e 's/\(COL-FBG.*:\).*/\1 #E3DAD5;/g' *.css
+sed -i -e 's/\(COL-HI.*:\).*/\1  #E3A45F;/g' *.css
+sed -i -e 's/\(COL-TXT.*:\).*/\1 #170741;/g' *.css
+sed -i -e 's/\(COL-WRN.*:\).*/\1 #E00606;/g' *.css
 
 
