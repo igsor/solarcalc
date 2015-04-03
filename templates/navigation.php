fc46850952e7f614767907fdf348ef8059129437
Matthias Baumgartner <dev@igsor.net>
2015-04-03 19:44:25 +0200

    page title as home-link

--- templates/navigation.php
+++ templates/navigation.php
@@ -3,11 +3,12 @@
 function t_menulink($trg, $name) {
     echo "<a class='menulink " . (basename($_SERVER['SCRIPT_NAME']) == $trg ? ' selected':'') . "' href='{$trg}'>${name}</a>";
 }
+
 ?>
 
 <div id="header">
 
-  <h1 id="pagetitle">Solar Installation Calculation Tool</h1>
+  <a href="index.php"><h1 id="pagetitle">Solar Installation Calculation Tool</h1></a>
   <?php
     t_menulink('load.php',              'LOAD');
     t_menulink('panel.php',             'PANEL');
@@ -18,3 +19,4 @@ function t_menulink($trg, $name) {
   <hr class="menuline">
 
 </div>
+
